4432b32cc5ca21503aaf25b19c11a1bcbcaf61ea
robmister2 <71057966+robmister2@users.noreply.github.com>
2021-01-26 16:17:47 -0700

    Add files via upload

---
+++ Lab 1 Lexer/MatcherAutomaton.h
@@ -0,0 +1,13 @@
+#include "Automaton.h"
+
+#ifndef UNTITLED_MATCHERAUTOMATON_H
+#define UNTITLED_MATCHERAUTOMATON_H
+
+
+class MatcherAutomaton {
+public: Automaton;
+
+};
+
+
+#endif //UNTITLED_MATCHERAUTOMATON_H
